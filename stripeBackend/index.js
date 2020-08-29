e91b3f071edd80651d55182695f941470f2f9809
eshoiza <engrjayt200@gmail.com>
2020-08-29 20:01:53 +0100

    created a simple server

--- stripeBackend/index.js
+++ stripeBackend/index.js
@@ -1,11 +1,13 @@
 const cors = require('cors');
 const express = require('express');
-//const stripe = require('stripe');
+const stripe = require("stripe")(
+  "pk_test_51GxF9FKtGo71ML3cKGTOBcTxZhnajgXajg86s5v2xBnwzeEQAWTIAWgkrkmjAp2x5cxv9ykvGEKyrtddzoyu6njt00pAoWmWqS"
+);
 const uuid = require("uuid/v4");
 
 const app= express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(cors())
 
 //Routes
@@ -13,5 +15,40 @@ app.get('/', (req, res)=>{
     res.send('It works great !!!')
 })
 
+
+app.post('/payment', (req, res)=>{
+    const {product, token} = req.body;
+    console.log("PRODUCT", product);
+    console.log("PRICE", product.price);
+    const idempotencyKey = uuid();
+
+    return stripe.customers.create({
+        email: token.eamil,
+        source: token.id
+    }).then(customer =>{
+        stripe.charges.create({
+            amount: product.price *100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `purchase of product.name`,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    country: token.card.address_country
+                }
+            }
+
+        }, {idempotencyKey})
+    })
+    .then(result =>res.status(200).json(result))
+    .catch(err = console.log(err))
+})
+
+
+
+
+
+
 //listen
-app.listen(4000, ()=> console.log("Listening on port 4000"));
+app.listen(2000, ()=> console.log("Listening on port 2000"));
